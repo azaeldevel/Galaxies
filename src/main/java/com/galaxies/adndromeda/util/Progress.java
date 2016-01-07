e356d60c4e3ab91529059bf10274da6ae688bc50
Azael Reyes <reyesazael12@gmail.com>
2016-01-07 13:25:04 -0800

    Creacion de proyecto.

---
+++ src/main/java/com/galaxies/adndromeda/util/Progress.java
@@ -0,0 +1,16 @@
+
+package com.galaxies.adndromeda.util;
+
+/**
+ * Para mustrar el prgreso de una actividad
+ * @author Azael Reyes
+ */
+public abstract class Progress implements Runnable
+{
+    /**
+     * Indica el estado actual de la actividad
+     * @param progress indicador numerico el estado actual
+     * @param message descripcion del paso realizado
+     */
+    public abstract void setProgress(int progress, String message);
+}
