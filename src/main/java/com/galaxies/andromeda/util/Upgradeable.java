71b023f274a3de2cb03bdbdcd2aed1c5ceeeed63
Azael Reyes <reyesazael12@gmail.com>
2016-02-23 13:21:57 -0800

    Nueva interface para inter¡actuar con objetos dinamicos.

---
+++ src/main/java/com/galaxies/andromeda/util/Upgradeable.java
@@ -0,0 +1,13 @@
+
+package com.galaxies.andromeda.util;
+
+/**
+ *
+ * @author Azael
+ */
+public interface Upgradeable 
+{
+    public void clean();
+    public void load();
+    public void reload();
+}
